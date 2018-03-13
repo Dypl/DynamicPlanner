0745977117674d576e5a1251d11216029c9fb012
jesumperez18 <jesuperez@csumb.edu>
2018-03-13 16:30:31 -0700

    Added pods/Parse and login

--- DynamicPlanner/AppDelegate.swift
+++ DynamicPlanner/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        // Initialize Parse
+        // Set applicationId and server based on the values in the Heroku settings.
+        // clientKey is not used on Parse open source unless explicitly configured
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Dynamic_Planner"
+                configuration.clientKey = "dwdadawdaedeadawddwajqwdhe1723"
+                // set to nil assuming you have not set clientKey
+                configuration.server = "https://dynami.herokuapp.com/parse"
+            })
+        )
+        
+        // This checks is a user is signed in already!
+//        if PFUser.current() != nil {
+//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//            // view controller currently being set in Storyboard as default will be overridden
+//            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+//        }
+        
         return true
+        
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DynamicPlanner' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DynamicPlanner
+    pod 'Parse'
+
+  target 'DynamicPlannerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'DynamicPlannerUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
