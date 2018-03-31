df24a569a15dac08f753ae4d4351f6ad7fe2e219
jesumperez18 <jesuperez@csumb.edu>
2018-03-30 17:44:29 -0700

    Completed A basic timer

--- DynamicPlanner/ViewController.swift
+++ DynamicPlanner/ViewController.swift
@@ -11,12 +11,18 @@ import UserNotifications
 
 class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
+    @IBOutlet weak var datePickerText: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Asked for permission
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in
         })
         UNUserNotificationCenter.current().delegate = self
+        createDatePicker()
         
         
     }
@@ -55,6 +61,21 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         content.body = "The 5 seconds are really up man"
         content.categoryIdentifier = "TIMER_EXPIRED"
         content.badge = 1
+        // Setting up Timer based on Calendar:
+        //Calendar: Trigger at a specific date and time. The trigger is created using a date components object which makes it easier for certain repeating intervals. To convert a Date to its date components use the current calendar.
+        
+        
+        
+     //  let date = Date(timeIntervalSinceNow: 3600)
+        
+//        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        //************************** Creating trigger from date components
+        
+       // let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        print("***********")
+       // print(trigger)
+        print("***********")
+        
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         let request = UNNotificationRequest(identifier:"timerDone", content: content, trigger:trigger)
@@ -81,6 +102,86 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         }
         
         completionHandler()
+    }
+    
+    func createDatePicker()
+    {
+        // Creating our scheduled based of UIPicker, this format will include year, month, day, hour and minutes, seconds
+
+        
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action:#selector(donePressed))
+        toolBar.setItems([doneButton], animated: false)
+        datePickerText.inputAccessoryView = toolBar
+        // assigning date picker to text field.
+        datePickerText.inputView = datePicker
+        
+    }
+    
+    @objc func donePressed()
+    {
+         let date = datePicker.date
+        
+        let generalCategory = UNNotificationCategory(identifier: "GENERAL",
+                                                     actions: [],
+                                                     intentIdentifiers: [],
+                                                     options: .customDismissAction)
+        
+        // Create the custom actions for the TIMER_EXPIRED category.
+        let snoozeAction = UNNotificationAction(identifier: "SNOOZE_ACTION",
+                                                title: "Snooze",
+                                                options: UNNotificationActionOptions(rawValue: 0))
+        let stopAction = UNNotificationAction(identifier: "STOP_ACTION",
+                                              title: "Stop",
+                                              options: .foreground)
+        
+        let expiredCategory = UNNotificationCategory(identifier: "TIMER_EXPIRED",
+                                                     actions: [snoozeAction, stopAction],
+                                                     intentIdentifiers: [],
+                                                     options: UNNotificationCategoryOptions(rawValue: 0))
+        
+        
+        //***** TO DO: Create an alarm clock that will determine the time the local notification must be displayed
+        let content = UNMutableNotificationContent()
+        content.title = "The 5 seconds are up!"
+        content.subtitle = "They are up now"
+        content.body = "The 5 seconds are really up man"
+        content.categoryIdentifier = "TIMER_EXPIRED"
+        content.badge = 1
+        
+        
+      
+        
+        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        //************************** Creating trigger from date components
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        print("*********** Picked time for notification")
+        print(trigger)
+        print("***********")
+        
+        //This line only applies to testing purposes, this is a 5 second response
+        //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier:"timerDone", content: content, trigger:trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+        
+        let center = UNUserNotificationCenter.current()
+        center.setNotificationCategories([generalCategory, expiredCategory])
+        
+        
+        
+        
+        //format date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        datePickerText.text = "\(datePicker.date)"
+        //datePickerText.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+        
     }
     
     
