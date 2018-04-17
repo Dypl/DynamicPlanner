525854ead908e05d277a262d2eaf866e73f6128a
Carlos Huizar-Valenzuela <carlos_huizar@Carloss-MacBook-Pro.local>
2018-04-17 12:37:40 -0700

    Recipe API call works

---
+++ DynamicPlanner/Recipe.swift
@@ -0,0 +1,23 @@
+//
+//  Recipe.swift
+//  DynamicPlanner
+//
+//  Created by Carlos Huizar-Valenzuela on 4/9/18.
+//  Copyright © 2018 Carlos Huizar-Valenzuela. All rights reserved.
+//
+
+import Foundation
+
+class Recipe {
+    
+    var imageURL: URL?
+    var ingredients: Array<Any>?
+    
+    init() {
+        imageURL = URL(string: "")
+        
+    }
+    
+    
+
+}
--- DynamicPlanner/ToDoViewController.swift
+++ DynamicPlanner/ToDoViewController.swift
@@ -12,7 +12,7 @@ class ToDoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
